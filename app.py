d7637a82dd8b6b1a0e48c5e97bb10650d341d7bb
izipay-pe <103148193+izipay-pe@users.noreply.github.com>
2025-01-09 08:51:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import json
 import hmac
 import hashlib
 from controller import dataForm
-from controller import calcularSignature
+from controller import checkSignature
 
 app = Flask(__name__)
 
@@ -25,70 +25,44 @@ def formulario():
     
     # Obtener todos los datos enviados por POST
     checkoutParameters = request.form.to_dict()
-    # Obtener el valor de la moneda y la cantidad
-    currency = checkoutParameters["currency"]
+    # Obtener la cantidad
     amount = checkoutParameters["amount"]
-    
-    # Asignar el tipo de moneda correspondiente
-    if (currency == "604"):
-        currencyType = "Soles"
-    else:
-        currencyType = "Dólares"
     
     # Calcular el Signature y los valores dinámicos para el formulario
     formParams = dataForm(checkoutParameters)
     
     # Renderiza el template enviando los valores para el formulario de pago
-    return render_template("checkout.html", parameters=formParams, currency=currencyType, amount=amount)
+    return render_template("checkout.html", parameters=formParams, amount=amount)
 
 
 # Manejo de solicitudes POST para la ruta result
 @app.post('/result')
 def paidResult():
-    
-    # Asignando los valores de la respuesta de Izipay en un diccionario
+    if not request.form: raise Exception("no post data received!")
+
     resultParameters = request.form.to_dict()
-    # Obtener el signature de la respuesta
-    resultPostSignature = resultParameters["signature"]
-    # Calcular el signature con los valores de la transacción
-    resultSignature = calcularSignature(resultParameters)
-    # Obtener el valor de la moneda
-    currency = resultParameters["vads_currency"]
+    
+    if not checkSignature(resultParameters) : raise Exception("Invalid signature")
+
     # Formatear los datos a Pretty Json
     pjson = json.dumps(resultParameters, indent=2, ensure_ascii=False)
     
-    # Asignar el tipo de moneda correspondiente
-    if (currency == "604"):
-        currencyType = "PEN"
-    else:
-        currencyType = "USD"
-    
-    # Verifica la integridad de el signature recibido y el generado
-    if resultPostSignature == resultSignature:
-        return render_template('result.html', data = resultParameters, pjson=pjson, currency=currencyType)
-    else:
-        return 'ERROR', 500
+    return render_template('result.html', data = resultParameters, pjson=pjson)
 
 # Manejo de solicitudes POST para la ruta ipn
 @app.post('/ipn')
 def ipn():
-    
-    # Asignando los valores de la respuesta de Izipay en un diccionario
+    if not request.form: raise Exception("no post data received!")
+
     ipnParameters = request.form.to_dict()
-    # Obtener el signature de la respuesta
-    ipnPostSignature = ipnParameters["signature"]
-    # Calcular el signature con los valores de la transacción
-    ipnSignature = calcularSignature(ipnParameters)
-    # Obtener el estado de la trasnacción
+    
+    if not checkSignature(ipnParameters) : raise Exception("Invalid signature")
+
+    # Obtener el estado de la transacción
     orderStatus = ipnParameters["vads_trans_status"]
     
-    # Verifica la integridad del signature recibido y el generado
-    if ipnPostSignature == ipnSignature:
-        print("OK! Order Status is " + orderStatus )
-        return 'Correcto', 200
-    else:
-        print("Notification Error")
-        return 'Acceso denegado', 500
+    # Retorna la respuesta del Order Status
+    return 'OK! OrderStatus is ' + orderStatus, 200
 
 if __name__ == '__main__':
     app.run(debug=True)
